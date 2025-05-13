b9b7793afeeae76eb96f3211e3faa7ba5eaa99a2
santanu <santanu.dutta@codelogicx.com>
2025-05-13 13:42:34 +0530

    Edit btn added

--- src/components/UserTableRow.jsx
+++ src/components/UserTableRow.jsx
@@ -1,23 +1,64 @@
-import React, { memo } from "react";
+import React, { useState, useRef, memo } from "react";
 
 const UserTableRow = memo(({ user, handleInputChange, handleKeyDown }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+
+  const shouldShowEditButton = isHovered && !isInputFocused;
+
+  const handleEditClick = (ref) => {
+    setIsInputFocused(true);
+    ref.current?.focus();
+  };
+
   return (
-    <tr>
-      <td>
+    <tr
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+        setIsInputFocused(false);
+      }}
+      style={{ position: "relative" }}
+    >
+      <td style={{ position: "relative" }}>
         <input
+          ref={nameInputRef}
           className="editable-input"
           value={user.name}
           onChange={(e) => handleInputChange(user.id, "name", e.target.value)}
           onKeyDown={handleKeyDown}
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
         />
+        {shouldShowEditButton && (
+          <button
+            className="edit-btn"
+            onClick={() => handleEditClick(nameInputRef)}
+          >
+            📝
+          </button>
+        )}
       </td>
-      <td>
+      <td style={{ position: "relative" }}>
         <input
+          ref={emailInputRef}
           className="editable-input"
           value={user.email}
           onChange={(e) => handleInputChange(user.id, "email", e.target.value)}
           onKeyDown={handleKeyDown}
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
         />
+        {shouldShowEditButton && (
+          <button
+            className="edit-btn"
+            onClick={() => handleEditClick(emailInputRef)}
+          >
+            📝
+          </button>
+        )}
       </td>
       <td>{user.company.name}</td>
     </tr>
